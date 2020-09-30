a4cb71b5146dd5f5f01ca0afd3545c9e3f693a68
akiakma <akiakmapro@gmail.com>
2020-09-30 20:04:33 +0900

    Updates

---
+++ precache-manifest.cc6e0e032c67e867f0ec92f539dbcf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ee0f07148df011a611b367ce0e4c89",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "b353cd1a41e571387826",
+    "url": "/habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "944bdbe893e00362c253",
+    "url": "/habit_tracker/static/js/2.c49bb654.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit_tracker/static/js/2.c49bb654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b353cd1a41e571387826",
+    "url": "/habit_tracker/static/js/main.849b6565.chunk.js"
+  },
+  {
+    "revision": "f5d4de07522f720b4809",
+    "url": "/habit_tracker/static/js/runtime-main.e70ce3e6.js"
+  }
+]);
